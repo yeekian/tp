aed55e48c9b7937bd3068fc44694378384260430
RCPilot1604 <ethan.chua1604@gmail.com>
2024-10-09 19:21:39 +0800

    Add modified StudentList class

---
+++ src/main/java/TutorLink/list/StudentList.java
@@ -0,0 +1,23 @@
+package tutorlink.list;
+
+import tutorlink.student.Student;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StudentList {
+    private Integer numberOfStudents;
+    private Student[] studentList = new Student[0];
+    ArrayList<Student> studentArrayList = new ArrayList<>(Arrays.asList(studentList));
+
+    public StudentList() {
+        this.numberOfStudents = 0;
+    }
+
+    public void deleteStudent(Student student){
+        studentArrayList.remove(student);
+    }
+
+    public void addStudent(Student student){
+        studentArrayList.add(student);
+    }
+}
