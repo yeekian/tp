ef712797f4f9ec86052e4dadaac469209fd9002f
RCPilot1604 <ethan.chua1604@gmail.com>
2024-10-09 18:55:16 +0800

    Add Attendance class

---
+++ src/main/java/TutorLink/attendance/Attendance.java
@@ -0,0 +1,23 @@
+package TutorLink.attendance;
+
+public class Attendance {
+    private String attendanceVenue;
+    private String attendanceDateTime; //modify this to accept LocalDateTime
+    private boolean isPresent;
+
+    public Attendance(String attendanceVenue, String localDateTime, boolean isPresent){
+        this.attendanceVenue = attendanceVenue;
+        this.attendanceDateTime = localDateTime;
+        this.isPresent = isPresent;
+    }
+
+    public String getAttendanceDateTime() {
+        return attendanceDateTime;
+    }
+    public String getAttendanceVenue() {
+        return attendanceVenue;
+    }
+    public boolean getIsPresent() {
+        return isPresent;
+    }
+}
