af24bd2bbeb9982b74e8b1759d836f60057f81fb
RCPilot1604 <ethan.chua1604@gmail.com>
2024-10-09 18:54:28 +0800

    Add Command.java

--- src/main/java/TutorLink/command/Command.java
+++ src/main/java/TutorLink/command/Command.java
@@ -1,4 +1,21 @@
 package TutorLink.command;
 
-public class Command {
+import java.util.ArrayList;
+
+/**
+ * Represents an interpreted Command from the user. A <code>Command</code> object corresponds to a
+ * single user-issued command from the terminal.
+ */
+
+abstract public class Command {
+    /**
+     * Executes the required operations to perform the command issued by the user.
+     */
+    abstract public void execute(ArrayList list);
+    /**
+     * Checks if the command is an exit command.
+     *
+     * @return whether the current command is an ExitCommand
+     */
+    abstract public boolean isExit();
 }
