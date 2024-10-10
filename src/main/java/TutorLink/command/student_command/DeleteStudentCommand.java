591814ef508f66569efa46f022c207c65ea7a668
yeekian <53704991+yeekian@users.noreply.github.com>
2024-10-10 23:28:58 +0800

    Add AddStudentCommand and DeleteStudentCommand

--- src/main/java/TutorLink/command/student_command/DeleteStudentCommand.java
+++ src/main/java/TutorLink/command/student_command/DeleteStudentCommand.java
@@ -1,16 +1,22 @@
-package tutorlink.command.student_command;
+package TutorLink.command.student_command;
 
+import TutorLink.list.List;
+import TutorLink.list.StudentList;
 import tutorlink.student.Student;
 
-import java.util.ArrayList;
-
-public class DeleteStudentCommand extends StudentCommand{
+public class DeleteStudentCommand extends StudentCommand {
 
     public DeleteStudentCommand(Student student) {
         super(student);
     }
-    @Override
-    public void execute(ArrayList list) {
 
+    // Implement the generic execute method to handle the List type
+    @Override
+    public void execute(List list) {
+        if (list instanceof StudentList) {
+            ((StudentList) list).deleteStudent(student);
+        } else {
+            throw new IllegalArgumentException("Invalid list type. Expected StudentList.");
+        }
     }
 }
--- src/main/java/TutorLink/command/student_command/addStudentCommand.java
+++ src/main/java/TutorLink/command/student_command/addStudentCommand.java
@@ -1,18 +1,29 @@
-package tutorlink.command.student_command;
+package TutorLink.command.student_command;
 
+import TutorLink.list.List;
 import tutorlink.student.Student;
+import TutorLink.list.StudentList;
 
 import java.util.ArrayList;
 
-public class addStudentCommand extends StudentCommand{
+public class AddStudentCommand extends StudentCommand {
 
-    public addStudentCommand(Student student) {
+    public AddStudentCommand(Student student) {
         super(student);
     }
 
+    // Implement the generic execute method to handle the List type
     @Override
-    public void execute(ArrayList list) {
-
+    public void execute(List list) {
+        if (list instanceof StudentList) {
+            ((StudentList) list).addStudent(student);
+        } else {
+            throw new IllegalArgumentException("Invalid list type. Expected StudentList.");
+        }
     }
 
+    @Override
+    public boolean isExit() {
+        return super.isExit();
+    }
 }
