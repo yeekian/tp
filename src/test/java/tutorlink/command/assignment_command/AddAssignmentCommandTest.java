f81ee0d924e22788a012b6205e17bcc6458d87b9
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-10-10 17:13:38 +0800

    Add basic JUnit test for AddAssignmentCommand

---
+++ src/test/java/tutorlink/command/assignment_command/AddAssignmentCommandTest.java
@@ -0,0 +1,23 @@
+package tutorlink.command.assignment_command;
+
+import org.junit.jupiter.api.Test;
+import tutorlink.assignment.Assignment;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddAssignmentCommandTest {
+    @Test
+    void execute_addOne_expectOne() {
+        // setup
+        ArrayList<Assignment> assignmentList = new ArrayList<>();
+        Assignment assignment1 = new Assignment("Test_1",100,100,0.2);
+        AddAssignmentCommand addAssignmentCommand = new AddAssignmentCommand(assignment1);
+        // execute
+        addAssignmentCommand.execute(assignmentList);
+        assertFalse(assignmentList.isEmpty());
+        assertEquals(1, assignmentList.size());
+        assertEquals(assignment1, assignmentList.get(0));
+    }
+}
