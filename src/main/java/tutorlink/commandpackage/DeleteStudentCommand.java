5b91b5b9d253f672607f3dcda236561b6ba492ef
RCPilot1604 <ethan.chua1604@gmail.com>
2024-10-12 18:48:13 +0800

    Modify execute() method to utilize students.filterList()

--- src/main/java/tutorlink/commandpackage/DeleteStudentCommand.java
+++ src/main/java/tutorlink/commandpackage/DeleteStudentCommand.java
@@ -2,50 +2,32 @@ package tutorlink.commandpackage;
 
 import tutorlink.exceptionspackage.IllegalValueException;
 import tutorlink.exceptionspackage.ItemNotFoundException;
+import tutorlink.listpackage.StudentList;
 import tutorlink.resultpackage.CommandResult;
 import tutorlink.studentpackage.StudentClass;
 
 import java.util.regex.Pattern;
 
-public class DeleteStudentCommand extends Command{
-
-    private String matricNumber;
+public class DeleteStudentCommand extends FindStudentCommand{
 
     private final String ERROR_MESSAGE = "Student %s not found";
     private final String SUCCESS_MESSAGE = "Student %s successfully deleted";
 
     public static final String COMMAND_WORD = "deletestudent";
 
-    public DeleteStudentCommand(String matricNumber) {
-        this.matricNumber = matricNumber;
+    public DeleteStudentCommand(String name, String matricNumber) {
+        super(name, matricNumber);
     }
 
     public CommandResult execute() {
-        try {
-            deleteStudent(this.matricNumber);
-            return new CommandResult(String.format(SUCCESS_MESSAGE, matricNumber));
-        } catch (IllegalValueException | ItemNotFoundException e) {
-            return new CommandResult(e.getMessage());
-        }
-    }
-    private void deleteStudent(String matricNumber) throws IllegalValueException, ItemNotFoundException {
-        String ErrMsg = "";
-        if (matricNumber.trim().isEmpty()) {
-            ErrMsg = "Empty Matric Number";
+        StudentList filteredList = students.filterList(this.name, this.matricNumber);
+        if(filteredList.getNumberOfStudents() > 0) {
+            filteredList.getStudentArrayList().remove(0); //remove the first match
+            return new CommandResult(SUCCESS_MESSAGE, filteredList);
         } else {
-            String regex = "^A\\d{7}[A-Z]$";
-            Pattern pattern = Pattern.compile(regex);
-            if (!pattern.matcher(matricNumber).matches()) {
-                ErrMsg = "Invalid Matric Number. Matric Number should be of the form A[0-9]{7}[A-Z]";
-            }
-        }
-        if (!ErrMsg.isEmpty()) {
-            throw new IllegalValueException(ErrMsg);
-        } else {
-            StudentClass student = new StudentClass(matricNumber);
-            if(!students.deleteStudent(student)) {
-                throw new ItemNotFoundException(String.format(ERROR_MESSAGE, matricNumber));
-            }
+            String identifier = (this.name != null ? this.name : "") + ", "
+                    + (this.name != null ? this.name : "");
+            return new CommandResult(String.format(ERROR_MESSAGE, identifier), filteredList);
         }
     }
 }
