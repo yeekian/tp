7a3b046617aeaae8cd111d843d1050e8b6e52f6e
RCPilot1604 <ethan.chua1604@gmail.com>
2024-10-12 18:25:11 +0800

    Modify Command

--- src/main/java/tutorlink/commandpackage/Command.java
+++ src/main/java/tutorlink/commandpackage/Command.java
@@ -1,22 +1,25 @@
 package tutorlink.commandpackage;
 
-import tutorlink.listpackage.ItemList;
+import tutorlink.listpackage.StudentList;
+import tutorlink.resultpackage.CommandResult;
 
 /**
  * Represents an interpreted Command from the user. A <code>Command</code> object corresponds to a
  * single user-issued command from the terminal.
  */
 public abstract class Command {
-    /**
-     * Executes the required operations to perform the command issued by the user.
-     */
-    public abstract void execute(ItemList list);
+
+    protected static StudentList students = new StudentList();
+    /** Executes the required operations to perform the command issued by the user. */
+    public abstract CommandResult execute();
 
     /**
      * Checks if the command is an exit command.
      *
      * @return whether the current command is an ExitCommand
      */
-    public abstract boolean isExit();
+    public boolean isExit(){
+        return false;
+    }
 }
 
