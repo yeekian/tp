f694bf9c4aad5a8da1a94d2ccda9fae8354b5080
RCPilot1604 <ethan.chua1604@gmail.com>
2024-10-13 15:45:24 +0800

    Modify error statements

--- src/main/java/tutorlink/commandpackage/FindStudentCommand.java
+++ src/main/java/tutorlink/commandpackage/FindStudentCommand.java
@@ -10,10 +10,12 @@ public class FindStudentCommand extends Command{
 
     public static final String COMMAND_WORD = "find_student";
     public static final String FORMAT_ERROR_MESSAGE = "Error, expected format: " + COMMAND_WORD
-            + " n/NAME AND/OR" + "i/MATRIC NUMBER";
-    public final static String REGEX = "^findstudent(?:\\s+n/([^\\\\s]+))?(?:\\s+i/(\\d{7}[a-zA-Z]?))?$";
+            + " n/NAME {AND/OR} " + "i/MATRIC NUMBER";
+    public static final int PREFIX_INDEX = 2;
+    public final static String REGEX = "^find_student (i/(A\\d{7}[A-Z])?( n/[\\w\\d]+)?|n/[\\w\\d]" +
+            "+( i/(A\\d{7}[A-Z])?)?)( )?$";
     private final String SUCCESS_MESSAGE = "Found the following students:";
-    private final String ERROR_MESSAGE = "No students found";
+    private final String ERROR_MESSAGE = "Student (%s) not found in Student List";
     public FindStudentCommand(String name, String matricNumber) {
         this.name = name;
         this.matricNumber = matricNumber;
@@ -25,8 +27,9 @@ public class FindStudentCommand extends Command{
         if(filteredList.getNumberOfStudents() > 0) {
             return new CommandResult(SUCCESS_MESSAGE, filteredList);
         } else {
-            String identifier = (this.name != null ? this.name : "") + ", "
-                    + (this.name != null ? this.name : "");
+            String identifier = (this.name != null ? this.name : "")
+                    + (this.matricNumber != null && this.name != null ? ", " : "")
+                    + (this.matricNumber != null ? "Matric no: " + this.matricNumber : "");
             return new CommandResult(String.format(ERROR_MESSAGE, identifier));
         }
     }
