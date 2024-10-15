b01aa27d61a7ef64093d9482d7498f21eb74974c
RCPilot1604 <ethan.chua1604@gmail.com>
2024-10-15 19:49:44 +0800

    Fix indentation

--- src/main/java/tutorlink/commandpackage/ListAssignmentCommand.java
+++ src/main/java/tutorlink/commandpackage/ListAssignmentCommand.java
@@ -3,26 +3,26 @@ package tutorlink.commandpackage;
 import tutorlink.coursepackage.Course;
 import tutorlink.resultpackage.CommandResult;
 
-    public class ListAssignmentCommand extends Command{
+public class ListAssignmentCommand extends Command{
 
-        public static final String COMMAND_WORD = "list_assignment";
-        public static final String FORMAT_ERROR_MESSAGE = "Error, expected format: " + COMMAND_WORD
-                + " i/MATRIC_NUMBER" + " c/COURSE_ID";
-        public static final String REGEX = "list_assignment\\s+i/(A\\d{7}[A-Z])\\s+c/([A-Z]{2,3}\\d{4})";
-        private static final String SUCCESS_MESSAGE = "Here are your assignments:";
-        private String matricNumber;
-        private String courseID;
+    public static final String COMMAND_WORD = "list_assignment";
+    public static final String FORMAT_ERROR_MESSAGE = "Error, expected format: " + COMMAND_WORD
+            + " i/MATRIC_NUMBER" + " c/COURSE_ID";
+    public static final String REGEX = "list_assignment\\s+i/(A\\d{7}[A-Z])\\s+c/([A-Z]{2,3}\\d{4})";
+    private static final String SUCCESS_MESSAGE = "Here are your assignments:";
+    private String matricNumber;
+    private String courseID;
 
-        public ListAssignmentCommand(String matricNumber, String courseID) {
-            this.matricNumber = matricNumber;
-            this.courseID = courseID;
-        }
+    public ListAssignmentCommand(String matricNumber, String courseID) {
+        this.matricNumber = matricNumber;
+        this.courseID = courseID;
+    }
 
-        @Override
-        public CommandResult execute() {
-            assert matricNumber != null;
-            assert courseID != null;
-            Course course = students.getStudent(this.matricNumber).courses.getCourse(this.courseID);
-            return new CommandResult(SUCCESS_MESSAGE,course.getAssignments());
-        }
+    @Override
+    public CommandResult execute() {
+        assert matricNumber != null;
+        assert courseID != null;
+        Course course = students.getStudent(this.matricNumber).courses.getCourse(this.courseID);
+        return new CommandResult(SUCCESS_MESSAGE,course.getAssignments());
+    }
 }
