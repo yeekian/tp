533f95861216b8ba1649e8790d8d5d1b73d6040c
yeekian <53704991+yeekian@users.noreply.github.com>
2024-10-15 17:58:48 +0800

    Create logger

--- src/main/java/tutorlink/TutorLink.java
+++ src/main/java/tutorlink/TutorLink.java
@@ -7,6 +7,10 @@ import tutorlink.resultpackage.CommandResult;
 import tutorlink.uipackage.Ui;
 import tutorlink.parserpackage.Parser;
 
+import java.io.IOException;
+import java.util.logging.*;
+import java.util.logging.Logger;
+
 /**
  * Represents the main class containing the entry point for the TutorLink application
  */
@@ -14,8 +18,15 @@ public class TutorLink {
     private static final Ui ui = new Ui();
     private static final Parser parser = new Parser();
 
+    private final static Logger LOGGER = Logger.getLogger(TutorLink.class.getName());
+
+
     /** Main entry-point for TutorLink */
     public static void main(String[] args) {
+
+        setUpLogger();
+        LOGGER.log(Level.INFO, "Test log message successful");
+
         Console.printWelcomeMessage();
         while(true) {
             try {
@@ -29,6 +40,23 @@ public class TutorLink {
             } catch (MissingNextLineException e) {
                 Console.printMessage(e.getMessage());
             }
+        }
+    }
+
+    private static void setUpLogger() {
+        LogManager.getLogManager().reset();
+        LOGGER.setLevel(Level.WARNING);
+        ConsoleHandler ch = new ConsoleHandler();
+        ch.setLevel(Level.INFO);
+        LOGGER.addHandler(ch);
+
+        try {
+            FileHandler fh = new FileHandler("myLogger.log");
+            fh.setLevel(Level.FINE);
+            LOGGER.addHandler(fh);
+        } catch (IOException e) {
+            //ignore
+            LOGGER.log(Level.SEVERE, "File logger not working", e);
         }
     }
 }
