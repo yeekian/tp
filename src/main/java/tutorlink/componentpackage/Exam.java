6189752cdb62aaf7d7af093839ba1cb3ff424000
Trung Bui <thetrung205@gmail.com>
2024-10-19 17:38:11 +0800

    Add class Exam

---
+++ src/main/java/tutorlink/componentpackage/Exam.java
@@ -0,0 +1,7 @@
+package tutorlink.componentpackage;
+
+public class Exam extends Component{
+    public Exam(String name, double maxScore, double weight) {
+        super(name, maxScore, weight);
+    }
+}
