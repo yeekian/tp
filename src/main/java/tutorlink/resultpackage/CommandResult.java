b0df73d35bdc1d287c0bbad03afd19257a7c594d
RCPilot1604 <ethan.chua1604@gmail.com>
2024-10-19 19:59:44 +0800

    Modify CommandResult

--- src/main/java/tutorlink/resultpackage/CommandResult.java
+++ src/main/java/tutorlink/resultpackage/CommandResult.java
@@ -7,20 +7,13 @@ public class CommandResult {
     private static final String LINE_SEPARATOR = "\n\t";
 
     public final String message;
-    public final ItemList returnedList;
 
     public CommandResult(String message) {
         this.message = message;
-        this.returnedList = null;
-    }
-
-    public CommandResult(String message, ItemList returnedList) {
-        this.message = message;
-        this.returnedList = returnedList;
     }
 
     @Override
     public String toString() {
-        return message + LINE_SEPARATOR + (returnedList == null ? "" : returnedList.toString());
+        return message;
     }
 }
