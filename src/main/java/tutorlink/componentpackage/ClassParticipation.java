e56db144a4fb0e94d89e9dc6e73b90de8d3bdbb3
Trung Bui <thetrung205@gmail.com>
2024-10-19 17:38:24 +0800

    Add class ClassParticipation

---
+++ src/main/java/tutorlink/componentpackage/ClassParticipation.java
@@ -0,0 +1,7 @@
+package tutorlink.componentpackage;
+
+public class ClassParticipation extends Component{
+    public ClassParticipation (String name, double maxScore, double weight) {
+        super(name, maxScore, weight);
+    }
+}
