2fc69869edebc2e86af8ebae92784831fb5faf81
Trung Bui <thetrung205@gmail.com>
2024-10-19 17:31:57 +0800

    Add Component

---
+++ src/main/java/tutorlink/componentpackage/Component.java
@@ -0,0 +1,22 @@
+package tutorlink.componentpackage;
+
+public class Component {
+    private String name;
+    private double maxScore;
+    private double weight;
+
+    public Component(String name, double maxScore, double weight) {
+        this.name = name;
+        this.maxScore = maxScore;
+        this.weight = weight;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getMaxScore() {
+        return maxScore;
+    }
+    public double getWeight() {
+        return weight;
+    }
+}
