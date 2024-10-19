dded0a858f67fcfe515619f208976a4b222e0499
Trung Bui <thetrung205@gmail.com>
2024-10-19 17:38:03 +0800

    Add class Assignment

---
+++ src/main/java/tutorlink/componentpackage/Assignment.java
@@ -0,0 +1,7 @@
+package tutorlink.componentpackage;
+
+public class Assignment extends Component {
+    public Assignment(String name, double maxScore, double weight) {
+        super(name, maxScore, weight);
+    }
+}
