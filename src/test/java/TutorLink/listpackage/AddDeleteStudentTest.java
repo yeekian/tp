4b09849b7d6ec5a6512c6fe48cea7deb40c17d3f
RCPilot1604 <ethan.chua1604@gmail.com>
2024-10-20 23:55:31 +0800

    Remove redundant test cases

--- src/test/java/TutorLink/listpackage/AddDeleteStudentTest.java
+++ src/test/java/TutorLink/listpackage/AddDeleteStudentTest.java
@@ -10,62 +10,24 @@ public class AddDeleteStudentTest {
 
     @Test
     public void addStudentCommand_nonEmptyMatricNumberAndGPA_success() throws Exception {
-        Parser parser = new Parser();
-        assertEquals(0, Command.getStudentList().getNumberOfStudents());
-        Command addStudentCommand = parser.addStudentCommand("add_student n/John i/A1234567X");
 
-        addStudentCommand.execute();
-        assertEquals(1, Command.getStudentList().getNumberOfStudents());
     }
 
     @Test
     public void deleteStudentCommand_onlyName_success() throws Exception {
-        Parser parser = new Parser();
-        assertEquals(0, Command.getStudentList().getNumberOfStudents());
-        Command addStudentCommand = parser.addStudentCommand("add_student n/John i/A1234567X");
 
-        addStudentCommand.execute();
-        assertEquals(1, Command.getStudentList().getNumberOfStudents());
-
-        //Test single name delete
-        Command deleteStudentCommand = parser.deleteStudentCommand("delete_student n/John");
-
-        deleteStudentCommand.execute();
-        assertEquals(0, Command.getStudentList().getNumberOfStudents());
 
     }
 
     @Test
     public void deleteStudentCommand_onlyMatricNumber_success() throws Exception {
-        Parser parser = new Parser();
-        assertEquals(0, Command.getStudentList().getNumberOfStudents());
-        Command addStudentCommand = parser.addStudentCommand("add_student n/John i/A1234567X");
 
-        addStudentCommand.execute();
-        assertEquals(1, Command.getStudentList().getNumberOfStudents());
-
-        //Test single name delete
-        Command deleteStudentCommand = parser.deleteStudentCommand("delete_student i/A1234567X");
-
-        deleteStudentCommand.execute();
-        assertEquals(0, Command.getStudentList().getNumberOfStudents());
 
     }
 
     @Test
     public void deleteStudentCommand_both_success() throws Exception {
-        Parser parser = new Parser();
-        assertEquals(0, Command.getStudentList().getNumberOfStudents());
-        Command addStudentCommand = parser.addStudentCommand("add_student n/John i/A1234567X");
 
-        addStudentCommand.execute();
-        assertEquals(1, Command.getStudentList().getNumberOfStudents());
-
-        //Test single name delete
-        Command deleteStudentCommand = parser.deleteStudentCommand("delete_student n/John i/A1234567X");
-
-        deleteStudentCommand.execute();
-        assertEquals(0, Command.getStudentList().getNumberOfStudents());
 
     }
 
