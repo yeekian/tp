2ea9c7f12a8547cd0889654ccf4089328107383d
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-10-22 23:36:38 +0800

    Add exception for adding duplicate component

--- src/main/java/tutorlink/lists/ComponentList.java
+++ src/main/java/tutorlink/lists/ComponentList.java
@@ -2,40 +2,54 @@ package tutorlink.lists;
 
 import tutorlink.component.Component;
 import tutorlink.exceptions.ComponentNotFoundException;
+import tutorlink.exceptions.DuplicateComponentException;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * Represents a list of components.
+ */
 public class ComponentList {
+    private static final String ERROR_COMPONENT_NOT_FOUND = "Error! No matching component found!";
+    private static final String ERROR_DUPLICATE_COMPONENT = "Error! Component already exists in the list!";
+
     private ArrayList<Component> componentArrayList;
 
     public ComponentList() {
         this.componentArrayList = new ArrayList<>();
     }
 
-    public ComponentList findComponent(String name) {
+    public ComponentList findComponent(String name) throws ComponentNotFoundException {
         ComponentList filteredList = new ComponentList();
         filteredList.componentArrayList = componentArrayList
                 .stream()
                 .filter(comp -> comp.getName().contains(name))
                 .collect(Collectors.toCollection(ArrayList::new));
         if (filteredList.componentArrayList.isEmpty()) {
-            throw new ComponentNotFoundException("No matching component found");
+            throw new ComponentNotFoundException(ERROR_COMPONENT_NOT_FOUND);
         }
         return filteredList;
     }
 
-    public void addComponent(Component component) {
+    public void addComponent(Component component) throws DuplicateComponentException {
+        for (Component comp : componentArrayList) {
+            if (comp.equals(component)) {
+                throw new DuplicateComponentException(ERROR_DUPLICATE_COMPONENT);
+            }
+        }
         componentArrayList.add(component);
     }
 
-    public void deleteComponent(Component component) {
+    public void deleteComponent(Component component) throws ComponentNotFoundException {
         for (Component comp : componentArrayList) {
-            if (comp == component) {
+            if (comp.equals(component)) {
                 componentArrayList.remove(comp);
+                return;
             }
         }
+        throw new ComponentNotFoundException(ERROR_COMPONENT_NOT_FOUND);
     }
 
     @Override
