df3cc1335c2cb1359ecb4166b96f3aca2e0fa21b
RCPilot1604 <ethan.chua1604@gmail.com>
2024-10-25 17:13:08 +0800

    Add all tests back into the correctly refactored folders

---
+++ src/test/java/tutorlink/command/AddStudentCommandTest.java
@@ -0,0 +1,53 @@
+//@@author RCPilot1604
+package tutorlink.command;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tutorlink.appstate.AppState;
+import tutorlink.exceptions.IllegalValueException;
+import tutorlink.result.CommandResult;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+public class AddStudentCommandTest {
+    private AppState appState;
+    private HashMap<String, String> arguments;
+    private AddStudentCommand command;
+
+    @BeforeEach
+    void setup() {
+        appState = new AppState();
+        arguments = new HashMap<>();
+        command = new AddStudentCommand();
+    }
+
+    @Test
+    void execute_addOne_expectOne() {
+        arguments.put("i/","A1234567X");
+        arguments.put("n/", "John");
+        CommandResult result = command.execute(appState, arguments);
+        assertNotNull(result);
+        assertEquals(result.toString(), "Student John (A1234567X) added successfully!");
+        assertEquals(appState.students.getStudentArrayList().size(), 1);
+    }
+
+    @Test
+    void execute_emptyInput_exceptionThrown() {
+        arguments.put("i/","A1234567X");
+        try {
+            CommandResult result = command.execute(appState, arguments);
+            fail("Expected an exception to be thrown due to empty input");
+        } catch (IllegalValueException e) {
+            // Assert that the exception message matches the expected outcome
+            assertEquals("Error! Either parameter passed is null!", e.getMessage());
+        } catch (Exception e) {
+            // If any other type of exception is thrown, fail the test
+            fail("Expected IllegalArgumentException, but got: " + e.getClass().getSimpleName());
+        }
+    }
+}
+//@@author
---
+++ src/test/java/tutorlink/parser/ParserTest.java
@@ -0,0 +1,160 @@
+//@@author yeekian
+package tutorlink.parser;
+
+import org.junit.jupiter.api.Test;
+import tutorlink.command.AddStudentCommand;
+import tutorlink.command.Command;
+import tutorlink.command.DeleteStudentCommand;
+import tutorlink.command.ExitCommand;
+import tutorlink.command.FindStudentCommand;
+import tutorlink.command.InvalidCommand;
+import tutorlink.command.ListStudentCommand;
+
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+
+    @Test
+    void  getCommand_addStudentCommand_addStudentCommandReturned() {
+
+        Parser parser = new Parser();
+        String input = "add_student i/A1234567X n/John";
+        Command actualCommand = parser.getCommand(input);
+
+        assertEquals(AddStudentCommand.class, actualCommand.getClass());
+    }
+
+    @Test
+    void  getCommand_deleteStudentCommand_deleteStudentCommandReturned() {
+        Parser parser = new Parser();
+        String input = "delete_student i/A1234567X";
+        Command actualCommand = parser.getCommand(input);
+
+        assertEquals(DeleteStudentCommand.class, actualCommand.getClass());
+    }
+
+    @Test
+    void  getCommand_exitCommand_exitCommandReturned() {
+        Parser parser = new Parser();
+        String input = "bye";
+        Command actualCommand = parser.getCommand(input);
+
+        assertEquals(ExitCommand.class, actualCommand.getClass());
+    }
+
+    @Test
+    void  getCommand_listStudentCommand_listStudentCommandReturned() {
+        Parser parser = new Parser();
+        String input = "list_student";
+        Command actualCommand = parser.getCommand(input);
+
+        assertEquals(ListStudentCommand.class, actualCommand.getClass());
+    }
+
+    @Test
+    void  getCommand_findStudentCommand_findStudentCommandReturned() {
+        Parser parser = new Parser();
+        String input = "find_student i/A1234567X n/John Doe";
+        Command actualCommand = parser.getCommand(input);
+
+        assertEquals(FindStudentCommand.class, actualCommand.getClass());
+    }
+
+    @Test
+    void  getCommand_invalidCommand_invalidCommandReturned() {
+        Parser parser = new Parser();
+        String input = "test_input";
+        Command actualCommand = parser.getCommand(input);
+
+        assertEquals(InvalidCommand.class, actualCommand.getClass());
+    }
+
+    @Test
+    void  getArguments_addStudentCommand_addStudentCommandHashMapReturned() {
+        Parser parser = new Parser();
+        String line = "add_student i/A1234567X n/John Doe";
+
+        Command currentCommand = new AddStudentCommand();
+        String[] argumentPrefixes = currentCommand.getArgumentPrefixes();
+
+        HashMap<String, String>  arguments = parser.getArguments(argumentPrefixes, line);
+
+        assertEquals(2, arguments.size());
+        assertEquals("A1234567X", arguments.get("i/")); // Check matriculation number
+        assertEquals("John Doe", arguments.get("n/")); // Check
+    }
+
+    @Test
+    void  getArguments_exitCommandNoArgumentPrefix_exitCommandHashMapReturned() {
+        Parser parser = new Parser();
+        String line = "bye";
+
+        Command currentCommand = new ExitCommand();
+        String[] argumentPrefixes = currentCommand.getArgumentPrefixes();
+
+        HashMap<String, String>  arguments = parser.getArguments(argumentPrefixes, line);
+
+        assertEquals(0, arguments.size());
+    }
+
+    @Test
+    void  getArguments_addStudentCommandExtraArguments_ignoreExtraTag() {
+        Parser parser = new Parser();
+        String line = "add_student i/A1234567X n/John Doe t/extraTag";
+
+        Command currentCommand = new AddStudentCommand();
+        String[] argumentPrefixes = currentCommand.getArgumentPrefixes();
+
+        HashMap<String, String>  arguments = parser.getArguments(argumentPrefixes, line);
+
+        assertEquals(2, arguments.size());
+        assertEquals("A1234567X", arguments.get("i/")); // Check matriculation number
+        assertEquals("John Doe", arguments.get("n/")); // Check
+    }
+
+    @Test
+    void  getArguments_addStudentCommandSwappedArguments_addStudentCommandHashMapReturned() {
+        Parser parser = new Parser();
+        String line = "add_student n/John Doe i/A1234567X ";
+
+        Command currentCommand = new AddStudentCommand();
+        String[] argumentPrefixes = currentCommand.getArgumentPrefixes();
+
+        HashMap<String, String>  arguments = parser.getArguments(argumentPrefixes, line);
+
+        assertEquals(2, arguments.size());
+        assertEquals("A1234567X", arguments.get("i/")); // Check matriculation number
+        assertEquals("John Doe", arguments.get("n/")); // Check
+    }
+
+    @Test
+    void  getArguments_addStudentCommandMissingArguments_hashMapWithOnlyGivenArgumentsReturned() {
+        Parser parser = new Parser();
+        String line = "add_student n/John Doe";
+
+        Command currentCommand = new AddStudentCommand();
+        String[] argumentPrefixes = currentCommand.getArgumentPrefixes();
+
+        HashMap<String, String>  arguments = parser.getArguments(argumentPrefixes, line);
+
+        assertEquals(1, arguments.size());
+        assertEquals("John Doe", arguments.get("n/")); // Check
+    }
+
+    @Test
+    void  getArguments_addStudentCommandInvalidArguments_emptyHashMapReturned() {
+        Parser parser = new Parser();
+        String line = "add_student j/test";
+
+        Command currentCommand = new AddStudentCommand();
+        String[] argumentPrefixes = currentCommand.getArgumentPrefixes();
+
+        HashMap<String, String>  arguments = parser.getArguments(argumentPrefixes, line);
+
+        assertEquals(0, arguments.size());
+    }
+}
+//@@author
