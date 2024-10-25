a6df46b69bba12382a88ccc39af2a15633c833e4
Trung Bui <thetrung205@gmail.com>
2024-10-25 17:37:52 +0800

    Add InvalidComponentException()

---
+++ src/main/java/tutorlink/exceptions/InvalidComponentException.java
@@ -0,0 +1,7 @@
+package tutorlink.exceptions;
+
+public class InvalidComponentException extends TutorLinkException {
+    public InvalidComponentException(String message) {
+        super(message);
+    }
+}
