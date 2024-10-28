6033ec09cf11b8ee669a2264f181826b8b43e017
Trung Bui <thetrung205@gmail.com>
2024-10-28 17:06:56 +0800

    Update ListStudentCommand and  ListStudentCommandTest

--- src/main/java/tutorlink/command/ListComponentCommand.java
+++ src/main/java/tutorlink/command/ListComponentCommand.java
@@ -3,25 +3,19 @@ package tutorlink.command;
 import java.util.HashMap;
 
 import tutorlink.appstate.AppState;
-import tutorlink.exceptions.TutorLinkException;
 import tutorlink.result.CommandResult;
 
 public class ListComponentCommand extends Command {
 
     public static final String COMMAND_WORD = "list_component";
-    private static final String SUCCESS_MESSAGE = "Here are your components:";
-    private static final String EMPTY_MESSAGE = "No components found.";
 
     @Override
-    public CommandResult execute(AppState appState, HashMap<String, String> parameters) throws TutorLinkException {
-        if (appState.components.size() == 0) {
-            return new CommandResult(EMPTY_MESSAGE);
-        }
-        return new CommandResult(SUCCESS_MESSAGE + "\n" + appState.components.toString());
+    public CommandResult execute(AppState appState, HashMap<String, String> parameters) {
+        return new CommandResult(appState.components.toString());
     }
 
     @Override
     public String[] getArgumentPrefixes() {
-        return new String[]{};
+        return null;
     }
 }
--- src/test/java/tutorlink/command/ListComponentCommandTest.java
+++ src/test/java/tutorlink/command/ListComponentCommandTest.java
@@ -1,18 +1,8 @@
 package tutorlink.command;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
-import java.util.HashMap;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import tutorlink.appstate.AppState;
-import tutorlink.component.Assignment;
-import tutorlink.component.Exam;
-import tutorlink.exceptions.TutorLinkException;
-import tutorlink.result.CommandResult;
 
 public class ListComponentCommandTest {
 
@@ -23,31 +13,5 @@ public class ListComponentCommandTest {
     public void setUp() {
         appState = new AppState();
         command = new ListComponentCommand();
-    }
-
-    @Test
-    void execute_emptyList_showsEmptyMessage() throws TutorLinkException {
-        CommandResult result = command.execute(appState, new HashMap<>());
-        assertEquals("No components found.", result.toString());
-    }
-
-    @Test
-    void execute_nonEmptyList_showsAllComponents() throws TutorLinkException {
-        appState.components.addComponent(new Assignment("Assignment 1", 50.0, 0.3));
-        appState.components.addComponent(new Exam("Midterm", 100.0, 0.4));
-
-        CommandResult result = command.execute(appState, new HashMap<>());
-
-        String expected = """
-                Here are your components:
-                1: Assignment [name=assignment 1, maxScore=50.0, weight=0.3]
-                \t2: Exam [name=midterm, maxScore=100.0, weight=0.4]""";
-        assertEquals(expected, result.toString());
-    }
-
-    @Test
-    void getArgumentPrefixes_returnsEmptyArray() {
-        assertArrayEquals(new String[]{}, command.getArgumentPrefixes(),
-                "ListComponentCommand should not take any arguments");
     }
 }
