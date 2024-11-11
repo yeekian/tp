ce7549dd18f7afff9e33fd8aef8fc91eb65aa05f
RCPilot1604 <ethan.chua1604@gmail.com>
2024-11-11 10:36:30 +0800

    Add more descriptive messages to ListComponent and ListStudent command

--- src/main/java/tutorlink/command/ListComponentCommand.java
+++ src/main/java/tutorlink/command/ListComponentCommand.java
@@ -8,9 +8,13 @@ import tutorlink.result.CommandResult;
 public class ListComponentCommand extends Command {
 
     public static final String COMMAND_WORD = "list_component";
+    private static final String MESSAGE_NO_COMPONENTS = "No components have been recorded yet.";
 
     @Override
     public CommandResult execute(AppState appState, HashMap<String, String> parameters) {
+        if(appState.components.size() <= 0) {
+            return new CommandResult(MESSAGE_NO_COMPONENTS);
+        }
         return new CommandResult(appState.components.toString());
     }
 
--- src/main/java/tutorlink/command/ListStudentCommand.java
+++ src/main/java/tutorlink/command/ListStudentCommand.java
@@ -8,10 +8,13 @@ import tutorlink.result.CommandResult;
 public class ListStudentCommand extends Command {
 
     public static final String COMMAND_WORD = "list_student";
-
+    private static final String MESSAGE_NO_STUDENTS = "No students have been recorded yet.";
 
     @Override
     public CommandResult execute(AppState appState, HashMap<String, String> hashMap) {
+        if(appState.students.size() <= 0) {
+            return new CommandResult(MESSAGE_NO_STUDENTS);
+        }
         return new CommandResult(appState.students.toString());
     }
 
