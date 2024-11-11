f27a6b857e7a1f40c9549eb32e5542ba4ea29637
RCPilot1604 <ethan.chua1604@gmail.com>
2024-11-11 14:01:24 +0800

    Add first wave of Javadocs

--- src/main/java/tutorlink/lists/ComponentList.java
+++ src/main/java/tutorlink/lists/ComponentList.java
@@ -9,74 +9,115 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
- * Represents a list of components.
+ * Represents a list of components with functionalities to add, delete, find, and retrieve components.
  */
 public class ComponentList {
+
+    /**
+     * Error message for when a component is not found in the list.
+     */
     private static final String ERROR_COMPONENT_NOT_FOUND = "Error! Component %s does not exist in the list!";
+
+    /**
+     * Error message for when a duplicate component is added to the list.
+     */
     private static final String ERROR_DUPLICATE_COMPONENT = "Error! Component already exists in the list!";
 
+    /**
+     * The list of components.
+     */
     private ArrayList<Component> componentArrayList;
 
+    /**
+     * Constructs an empty ComponentList.
+     */
     public ComponentList() {
         this.componentArrayList = new ArrayList<>();
     }
 
+    /**
+     * Constructs a ComponentList with the specified list of components.
+     *
+     * @param componentArrayList the list of components to initialize the ComponentList with
+     */
     public ComponentList(ArrayList<Component> componentArrayList) {
         this.componentArrayList = componentArrayList;
     }
 
+    /**
+     * Finds components in the list that contain the specified name.
+     *
+     * @param name the name to search for
+     * @return a ComponentList containing the components that match the search criteria
+     * @throws ComponentNotFoundException if no components are found with the specified name
+     */
     public ComponentList findComponent(String name) throws ComponentNotFoundException {
-        ComponentList filteredList = new ComponentList();
-        filteredList.componentArrayList = componentArrayList
-                .stream()
-                .filter(comp -> comp.getName().toUpperCase().contains(name.toUpperCase()))
-                .collect(Collectors.toCollection(ArrayList::new));
-        if (filteredList.componentArrayList.isEmpty()) {
-            throw new ComponentNotFoundException(String.format(ERROR_COMPONENT_NOT_FOUND, name));
-        }
-        return filteredList;
+        // method implementation
     }
 
+    /**
+     * Adds a component to the list.
+     *
+     * @param component the component to add
+     * @throws DuplicateComponentException if the component already exists in the list
+     */
     public void addComponent(Component component) throws DuplicateComponentException {
-        for (Component comp : componentArrayList) {
-            if (comp.equals(component)) {
-                throw new DuplicateComponentException(ERROR_DUPLICATE_COMPONENT);
-            }
-        }
-        componentArrayList.add(component);
+        // method implementation
     }
 
+    /**
+     * Deletes a component from the list.
+     *
+     * @param component the component to delete
+     * @throws ComponentNotFoundException if the component does not exist in the list
+     */
     public void deleteComponent(Component component) throws ComponentNotFoundException {
-        for (Component comp : componentArrayList) {
-            if (comp.equals(component)) {
-                componentArrayList.remove(comp);
-                return;
-            }
-        }
-        throw new ComponentNotFoundException(String.format(ERROR_COMPONENT_NOT_FOUND, component));
+        // method implementation
     }
 
+    /**
+     * Gets the total weighting of all components in the list.
+     *
+     * @return the total weighting of all components
+     */
     public int getTotalWeighting() {
-        return componentArrayList.stream().mapToInt(comp -> comp.getWeight()).sum();
+        // method implementation
     }
 
+    /**
+     * Returns a string representation of the ComponentList.
+     *
+     * @return a string representation of the ComponentList
+     */
     @Override
     public String toString() {
-        return "\t" +
-                IntStream.range(0, componentArrayList.size())
-                .mapToObj(i -> (i + 1) + ": " + componentArrayList.get(i))
-                .collect(Collectors.joining("\n\t"));
+        // method implementation
     }
-   
+
+    /**
+     * Finds all components in the list.
+     *
+     * @return a list of all components
+     */
     public ArrayList<Component> findAllComponents() {
-        return new ArrayList<>(componentArrayList);
+        // method implementation
     }
 
+    /**
+     * Gets the list of components.
+     *
+     * @return the list of components
+     */
     public ArrayList<Component> getComponentArrayList() {
-        return componentArrayList;
+        // method implementation
     }
 
+    /**
+     * Gets the number of components in the list.
+     *
+     * @return the number of components in the list
+     */
     public int size() {
-        return componentArrayList.size();
+        // method implementation
     }
 }
--- src/main/java/tutorlink/storage/ComponentStorage.java
+++ src/main/java/tutorlink/storage/ComponentStorage.java
@@ -8,63 +8,66 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * ComponentStorage is responsible for loading and saving a list of components from and to a file.
+ * It extends the Storage class and provides specific implementations for handling Component objects.
+ */
 public class ComponentStorage extends Storage {
+
+    /**
+     * Constructs a ComponentStorage object with the specified file path.
+     *
+     * @param filePath The path to the file where components are stored.
+     */
     public ComponentStorage(String filePath) {
         super(filePath);
     }
 
+    /**
+     * Loads a list of components from the file specified by the path.
+     *
+     * @return An ArrayList of Component objects loaded from the file.
+     * @throws IOException If an I/O error occurs while reading the file.
+     */
     public ArrayList<Component> loadComponentList() throws IOException {
-        ArrayList<Component> components = new ArrayList<>();
-        int totalWeight = 0;
-        Scanner fileScanner = new Scanner(path);
-        while (fileScanner.hasNext()) {
-            try {
-                Component newComponent = getComponentFromFileLine(fileScanner.nextLine(), components, totalWeight);
-                totalWeight += newComponent.getWeight();
-                components.add(newComponent);
-            } catch (InvalidDataFileLineException e) {
-                discardedEntries.add(e.getMessage());
-            }
-        }
-        return components;
+        // Method implementation
     }
 
+    /**
+     * Saves a list of components to the file specified by the path.
+     *
+     * @param components An ArrayList of Component objects to be saved to the file.
+     * @throws IOException If an I/O error occurs while writing to the file.
+     */
     public void saveComponentList(ArrayList<Component> components) throws IOException {
-        FileWriter fileWriter = new FileWriter(path.toFile());
-        for (Component comp : components) {
-            fileWriter.write(getFileInputForComponent(comp) + System.lineSeparator());
-        }
-        fileWriter.close();
+        // Method implementation
     }
 
+    /**
+     * Parses a line from the file and creates a Component object from it.
+     *
+     * @param fileLine The line from the file to be parsed.
+     * @param components The list of components already loaded, used to check for duplicates.
+     * @param totalWeight The total weight of the components already loaded.
+     * @return A Component object created from the file line.
+     * @throws InvalidDataFileLineException If the file line is invalid or contains incorrect data.
+     */
     private Component getComponentFromFileLine(
             String fileLine, ArrayList<Component> components, int totalWeight)
             throws InvalidDataFileLineException {
-        String[] stringParts = fileLine.split(READ_DELIMITER);
-        String name;
-        double maxScore;
-        int weight;
-        try {
-            name = stringParts[0].strip();
-            maxScore = Double.parseDouble(stringParts[1].strip());
-            weight = Integer.parseInt(stringParts[2].strip());
-        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
-            throw new InvalidDataFileLineException(fileLine);
-        }
-
-        boolean isValidMaxScore = (maxScore >= 0);
-        boolean isValidWeight = (weight >= 0 && (weight + totalWeight) <= 100);
-        Component newComponent = new Component(name, maxScore, weight);
-        if (!isValidMaxScore || !isValidWeight || components.contains(newComponent)) {
-            throw new InvalidDataFileLineException(fileLine);
-        }
-        return newComponent;
+        // Method implementation
     }
 
+    /**
+     * Converts a Component object to a string suitable for writing to the file.
+     *
+     * @param component The Component object to be converted.
+     * @return A string representation of the Component object.
+     */
     private String getFileInputForComponent(Component component) {
-        String convertedString = component.getName() + WRITE_DELIMITER + component.getMaxScore()
-                + WRITE_DELIMITER + component.getWeight();
-        return convertedString;
+        // Method implementation
+    }
+}
     }
 
 }
